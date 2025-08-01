f2933c21630474914355090f75a0447b92bb0ede
Navyavallapu <navyavallapu110@gmail.com>
2025-08-01 13:07:20 +0530

    commit

--- src/components/ShortlistForm.js
+++ src/components/ShortlistForm.js
@@ -59,6 +59,7 @@ const ShortlistForm = () => {
         </select>
         <button
           type="submit"
+          onClick={handleSubmit}
           className="bg-purple-600 text-white px-4 py-2 rounded hover:bg-purple-700"
         >
           Submit Status
