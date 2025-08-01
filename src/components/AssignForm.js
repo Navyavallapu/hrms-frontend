5a0f8b8d3cc140c742eea1d564ea690a647db796
Navyavallapu <navyavallapu110@gmail.com>
2025-08-01 12:57:26 +0530

    commit

--- src/components/AssignForm.js
+++ src/components/AssignForm.js
@@ -57,6 +57,7 @@ const AssignForm = () => {
         />
         
         <button
+          onClick={handleSubmit}
           type="submit"
           className="bg-purple-600 text-white px-4 py-2 rounded hover:bg-purple-700"
         >
